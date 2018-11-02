5b53b96344bf1c37052ebda25aa3dc45aab1e477
16110275 <39640931+16110275@users.noreply.github.com>
2018-11-02 13:51:31 +0700

    16110275

---
+++ MovieBox-master/app/src/main/java/com/example/baris/moviebox/Movie.java
@@ -0,0 +1,19 @@
+package com.example.baris.moviebox;
+
+import com.example.baris.moviebox.utilities.NetworkUtils;
+
+
+
+public class Movie {
+    String title , posterImageLink , synopsis , rating, releaseDate ;
+
+    public Movie(String _title, String _posterImage , String _synopsis ,String _rating , String _releaseDate){
+        title = _title ;
+        posterImageLink = NetworkUtils.getFullLink(_posterImage);
+        synopsis = _synopsis ;
+        rating = _rating ;
+        releaseDate = _releaseDate ;
+    }
+
+
+}
